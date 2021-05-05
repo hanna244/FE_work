b48c5146314c2d296127742ed933ca3ec9a146f5
Hanna Kim <shhn0509@gmail.com>
2021-05-05 16:45:51 +0900

    [스타일] 스타일 초기화 및 전역 스타일 설정
    - reset 스타일
    - 전역 스타일
    - sanitize.css 적용
    - mixin, function 생성

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
 import App from 'app/App'
 
+import './styles/index.scss'
+
 import 'react-app-polyfill/ie9'
 import 'react-app-polyfill/stable'
 
@@ -14,10 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// 서비스 워커 설정
-if (process.env.NODE_ENV === 'production') {
-  import('./serviceWorker')
-    .then((serviceWorker) => serviceWorker.register)
-    .catch((error) => console.error(error.message))
-}
